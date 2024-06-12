d3b3a34272c95bebf6f95d33f3cd7b5b6e7ad388
CarlosEDsl <carloseduardoestds@gmail.com>
2024-06-12 14:53:56 -0300

    Small refacturing at createComponent using lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component'
-import { CreateComponent } from './features/create/create.component';
 
 export const routes: Routes = [
   {
@@ -9,5 +8,5 @@ export const routes: Routes = [
 },
 {
   path: 'input-project',
-  component: CreateComponent,
+  loadComponent: () => import('./features/create/create.component').then(m => m.CreateComponent),
 }];
