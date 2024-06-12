71d9791354476f1a2b4d5869c83e2cc280c34d00
CarlosEDsl <carloseduardoestds@gmail.com>
2024-06-12 14:34:28 -0300

    Adding 'creating project' button

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -3,11 +3,13 @@ import { Component, inject } from '@angular/core';
 import { ProjectsService } from '../../shared/services/projects.service'
 import { Project } from '../../shared/interfaces/projecttInterface';
 import { CardComponent } from './components/card/card.component'
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
