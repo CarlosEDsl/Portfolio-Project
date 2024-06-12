534a1e4422765fb30a83ec8cac2cb93eb550dda8
CarlosEDsl <carloseduardoestds@gmail.com>
2024-06-12 14:47:59 -0300

    Router to input-component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component'
+import { CreateComponent } from './features/create/create.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {
   path: '',
   component: ListComponent
+},
+{
+  path: 'input-project',
+  component: CreateComponent,
 }];
