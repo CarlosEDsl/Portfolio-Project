7b02825b31d402f1e335f19936a93afaf5bc0349
CarlosEDsl <carloseduardoestds@gmail.com>
2024-06-13 17:30:09 -0300

    stating scss in form

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
+
 export class CreateComponent {
   form = new FormGroup({
     title: new FormControl<string>('', {
@@ -23,7 +25,8 @@ export class CreateComponent {
   });
 
   onSubmit() {
-
+    this.form.controls.title.value;
+    this.form.controls.shortDesc.value;
   }
 
 }
