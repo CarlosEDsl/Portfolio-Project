e7127008c2e1e791aa2de9efd706a6b8755b28bc
CarlosEDsl <carloseduardoestds@gmail.com>
2024-06-19 16:40:17 -0300

    Edit component refactored

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -1,16 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProjectsService } from '../../shared/services/projects.service';
+import { FormComponent } from '../../shared/components/form/form.component';
+import { Project } from '../../shared/interfaces/projecttInterface';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormComponent],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
@@ -21,36 +19,15 @@ export class EditComponent {
   projectsService = inject(ProjectsService);
   project = inject(ActivatedRoute).snapshot.data['project'];
   
-  form = new FormGroup({
-    title: new FormControl<string>('', {
-      nonNullable: true,
-      validators: Validators.required
-    }),
-    shortDesc: new FormControl<string>('', {
-      nonNullable: false,
-      validators: Validators.maxLength(60)
-    }),
-    description: new FormControl<string>('', {
-      nonNullable: false,
-      validators: Validators.maxLength(200)
-    }),
-    url: new FormControl<string>('', {
-      nonNullable: true,
-      validators: Validators.required
-    }),
-    img: new FormControl<string>('', {
-      nonNullable: false,
-    })
-  });
 
-  onSubmit() {
+  onSubmit(project:Project) {
     this.projectsService.put({
       id: this.project.id,
-      title: this.form.controls.title.value,
-      shortDesc: this.form.controls.shortDesc.value,
-      description: this.form.controls.description.value,
-      url: this.form.controls.url.value,
-      img: this.form.controls.img.value
+      title: project.title,
+      shortDesc: project.shortDesc,
+      description: project.description,
+      url: project.url,
+      img: project.img
     })
     .subscribe(() => {
       this.matSnackBar.open("Atualizado com sucesso", "") as MatSnackBarConfig;
