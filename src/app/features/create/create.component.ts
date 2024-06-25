438011eb34b778b518b268167aea1a8cbc16a8ac
CarlosEDsl <carloseduardoestds@gmail.com>
2024-06-25 17:33:47 -0300

    Projects display organizated

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -2,14 +2,15 @@ import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ProjectsService } from '../../shared/services/projects.service';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar'
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormComponent } from '../../shared/components/form/form.component';
 import { Project } from '../../shared/interfaces/projecttInterface';
+import { BackToHomeComponent } from '../../shared/components/back-to-home/back-to-home.component';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, BackToHomeComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
